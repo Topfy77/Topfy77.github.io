5e061e1adbb5adbee366e5a83797424538e6bc57
Topfy77 <topfyzdml@gmail.com>
2025-11-03 03:49:28 +0700

    neqqqqq

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
 
 export default defineConfig({
-  plugins: [
-    vue(),
-    vuetify({ autoImport: true }), 
-  ],
+  plugins: [vue()],
+  base: '/project_Bus/', // 👈 ต้องตรงกับชื่อ repo บน GitHub
 })
